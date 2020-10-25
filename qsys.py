aaa17cfaaf8eccaf857384148d553c6d3087f25e
mghelton <gagehelton@gmail.com>
2020-10-25 10:41:28 -0400

    remove trash test

--- qsys.py
+++ qsys.py
@@ -321,31 +321,5 @@ class LoopPlayerControl(Base): #coming soon
         pass
 
 if __name__ == '__main__':
-    core = Core(User='',Password='',ip='192.168.61.2')
-    core.start()
-
-    #gain = Control(parent=core,Name='gain',ValueType=int)
-    
-    cg = ChangeGroup(parent=core,Id='mygroup')
-
-    #create some control objects
-    for i in range(1,10):
-        l = Control(parent=core,Name='Mixer6x9Output{}Label'.format(i),ValueType=str)
-        cg.AddControl(l)
-        m = Control(parent=core,Name='Mixer6x9Output{}Mute'.format(i),ValueType=[int,float])
-        cg.AddControl(m)
-        g = Control(parent=core,Name='Mixer6x9Output{}Gain'.format(i),ValueType=[int,float])
-        cg.AddControl(g)
-        print(l,m,g)
-
-    time.sleep(2)
-
-    cg.AutoPoll(Rate=.1)
-
-    while True:
-        x = str(input("Control Name: "))
-        if(x):
-            try:
-                print(core.Objects[x].state)
-            except:
-                print("fail")
+    from test import main
+    main()
--- test.py
+++ test.py
@@ -1,32 +1,34 @@
+#!/usr/bin/python3
 import time
 from qsys import Core,Control,ChangeGroup
 
-#See qsys.py for parameters in Core class
-#The initiail EngineStatus response parameters from the device will get added to Core.__dict__
-#You can pass "port" as well, but it defaults to 1710
-myCore = Core(User='',Password='',ip='192.168.61.2')
+def main():
+    #See qsys.py for parameters in Core class
+    #The initiail EngineStatus response parameters from the device will get added to Core.__dict__
+    #You can pass "port" as well, but it defaults to 1710
+    myCore = Core(User='',Password='',ip='192.168.61.2')
 
-#Open the socket,create a "listen" thread
-myCore.start()
+    #Open the socket,create a "listen" thread
+    myCore.start()
 
-#ValueType can be a list of potential value types [int,float] or a single type "str" etc
-#This object is assumed to be a "gain" control object, so we can pass [int,float]
-gainControlObject = Control(parent=myCore,Name='namedControlInQsysDesigner',ValueType=[int,float])
+    #ValueType can be a list of potential value types [int,float] or a single type "str" etc
+    #This object is assumed to be a "gain" control object, so we can pass [int,float]
+    gainControlObject = Control(parent=myCore,Name='namedControlInQsysDesigner',ValueType=[int,float])
 
-#To constantly monitor the state of your object use a ChangeGroup
-#You need to a ChangeGroup instance to add control objects and set polling rates
-#Parameters that are capitalize are that way because of the QRC parameter protocol
-#Id in this case is just the name of the ChangeGroup
-myChangeGroup = ChangeGroup(parent=myCore,Id='myChangeGroup')
-myChangeGroup.AddControl(gainControlObject)
+    #To constantly monitor the state of your object use a ChangeGroup
+    #You need to a ChangeGroup instance to add control objects and set polling rates
+    #Parameters that are capitalize are that way because of the QRC parameter protocol
+    #Id in this case is just the name of the ChangeGroup
+    myChangeGroup = ChangeGroup(parent=myCore,Id='myChangeGroup')
+    myChangeGroup.AddControl(gainControlObject)
 
-#Allow the socket time to connect and parse the initial responses
-time.sleep(2)
+    #Allow the socket time to connect and parse the initial responses
+    time.sleep(2)
 
-#Set the change group auto poll rate
-#This rate is fast, your mileage may vary
-myChangeGroup.AutoPoll(Rate=0.1)
+    #Set the change group auto poll rate
+    #This rate is fast, your mileage may vary
+    myChangeGroup.AutoPoll(Rate=0.1)
 
-while True:
-    print(gainControlObject.state)
-    time.sleep(1)
+    while True:
+        print(gainControlObject.state)
+        time.sleep(1)
