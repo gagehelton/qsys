9303b9efdc9c93f3be20a5eda5502beffd5006f3
mghelton <gagehelton@gmail.com>
2020-10-30 18:45:16 -0400

    i hate js

--- qsys/ui/static/js/application.js
+++ qsys/ui/static/js/application.js
@@ -1,36 +1,41 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
 var controls = [];
 
-class controlObject {
+class volDiv {
 	constructor(name) {
 		this.name=name;
-		this.value=0;
-		//this.div = `<div class="progress" id="${this.name}"> 
-		//<div class="progress-bar" role="progressbar" aria-valuenow="${this.value}" aria-valuemin="-100" aria-valuemax="20"></div>
-		//</div>`;
 		this.element = document.createElement('div');
 		this.element.id = this.name;
 		this.element.className = "progress";
-		//this.element.innerHTML =   `<div id="bar" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="-100" aria-valuemax="20"></div>`;
 		this.sub_element = document.createElement('div');
-		this.sub_element.id = this.name+'_sub';
+		this.sub_element.id = this.name+'_progressbar';
 		this.sub_element.setAttribute('class', 'progress-bar progress-bar-striped progress-bar-animated');
 		this.sub_element.setAttribute('role','progressbar');
 		this.sub_element.setAttribute('aria-valuenow',this.value);
 		this.sub_element.setAttribute('aria-valuemin','-100');
 		this.sub_element.setAttribute('aria-valuemax','20');
 		this.sub_element.setAttribute('style','width: 75%');
-
 		this.element.appendChild(this.sub_element);
-		this.node = document.body.appendChild(this.element);
+		//this.node = document.body.appendChild(this.element);
+		//document.body.appendChild(document.createElement('br'));
+	}
+}
+
+class controlObject {
+	constructor(name) {
+		this.name=name;
+		this.value=0;
+		this.ui_element = new volDiv(this.name);
+		document.body.appendChild(this.ui_element.element);
 		document.body.appendChild(document.createElement('br'));
+		
 	}
 
 	listen() {
 		let self = this;
 		socket.on(this.name,function(msg) {
 			self['value'] = msg.value;
-			document.getElementById(self.name+"_sub").setAttribute('style','width: '+self.value+'%');
+			document.getElementById(self.name+"_progressbar").setAttribute('style','width: '+self.value+'%');
 			console.log(self.name+'-'+self.value);
 		});
 	}
